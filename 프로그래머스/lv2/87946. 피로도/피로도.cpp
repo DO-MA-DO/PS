6a368adb1227af2272a8c66377610dc4f005ed8b
DOMADO <62499126+DO-MA-DO@users.noreply.github.com>
2023-01-03 19:20:25 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.cpp
+++ 프로그래머스/lv2/87946. 피로도/피로도.cpp
@@ -3,42 +3,30 @@
 
 using namespace std;
 
-bool is_in[8];
-vector<int> nPm;
+bool check[8];
+
 int K;
 int dn;
 int max_cnt = -1;;
 
-void per(vector<vector<int>> dungeons) {
-    if(nPm.size() == dn) {
-        //선택된 순서로 던전 
-        int cur = K;
-        int cnt = 0;
-        for(int i: nPm) {
-            if(dungeons[i][0] > cur) break;
-            cur -= dungeons[i][1];
-            cnt++;
+void dfs(int cur, int cnt,  vector<vector<int>> dungeons) {
+    if(cnt > max_cnt) max_cnt = cnt;
+    
+    for(int i = 0; i<dungeons.size(); i++) {
+        if(cur >= dungeons[i][0] && !check[i]) {
+            check[i] = true;
+            dfs(cur-dungeons[i][1], cnt+1, dungeons);
+            check[i] = false;
         }
-        if(cnt > max_cnt) max_cnt = cnt;
-        return;
-    }
-    
-    
-    for(int i = 0; i<dn; i++) {
-        if(is_in[i]) continue;
-        is_in[i] = true;
-        nPm.push_back(i);
-        per(dungeons);
-        nPm.pop_back();
-        is_in[i] = false;
     }
 }
+
 
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = -1;
     K=k;
     dn = dungeons.size();
-    per(dungeons);
+    dfs(k, 0, dungeons);
     answer = max_cnt;
     return answer;
 }

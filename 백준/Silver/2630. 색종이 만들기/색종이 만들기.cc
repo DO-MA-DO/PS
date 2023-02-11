6932d1bf4c5a7fc4a8237ede5ff3b79a9700a120
DOMADO <62499126+DO-MA-DO@users.noreply.github.com>
2023-02-11 17:31:25 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,49 +1,49 @@
 #include <iostream>
+using namespace std;
 
-int arr[128][128] = { 0, };
-int N;
-int blue = 0;
-int white = 0;
+int cnt_blue = 0;
+int cnt_white = 0;
 
-void df(int x, int y, int n) {
+bool is_blue[128][128];
 
-	int half = n / 2;
-	
-	int cnt = 0;
-	for (int i = x; i < x + n; i++) {
-		for (int j = y; j < y + n; j++) {
-			if (arr[i][j]) cnt++;
+void divide(int si, int sj, int n) {
+	if (n == 0) return;
+	bool all_blue = true;
+	bool all_white = true;
+	for (int i = si; i < si + n; i++) {
+		for (int j = sj; j < sj + n; j++) {
+			if (is_blue[i][j]) {
+				all_white = false;
+			}
+			else {
+				all_blue = false;
+			}
 		}
 	}
-
-	if (cnt == 0) {
-		white++;
-	}
-	else if (cnt == n * n) {
-		blue++;
-	}
-	else {
-		df(x, y, half);
-		df(x + half, y, half);
-		df(x, y + half, half);
-		df(x + half, y + half, half);
+	if (all_blue) {
+		cnt_blue++;
 		return;
 	}
-
+	else if (all_white) {
+		cnt_white++;
+		return;
+	}
+	//divide 하러 가야지~
+	int half = n / 2;
+	divide(si, sj, half);
+	divide(si, sj + half, half);
+	divide(si + half, sj, half);
+	divide(si + half, sj + half, half);
 }
 
 int main() {
-	
+	int N;
 	scanf("%d", &N);
-
-	int in;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			scanf("%d", &in);
-			if (in) arr[i][j] = 1;
+			scanf("%d", &is_blue[i][j]);
 		}
 	}
-
-	df(0, 0, N);
-	printf("%d\n%d", white, blue);
+	divide(0, 0, N);
+	printf("%d\n%d", cnt_white, cnt_blue);
 }

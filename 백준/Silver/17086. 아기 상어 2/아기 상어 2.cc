6b57a7c805c69cf5be1238950bed762d2777de0f
DOMADO <62499126+DO-MA-DO@users.noreply.github.com>
2023-03-03 22:15:38 +0900

    [Silver II] Title: 아기 상어 2, Time: 84 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
@@ -5,44 +5,43 @@ using namespace std;
 int N, M;
 bool map[51][51];
 bool visit[51][51];
-int didj[8][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1} };
+int didj[8][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1},{1, 1}, {-1, -1}, {1, -1}, {-1, 1} };
+
+struct info {
+	int i, j, cnt;
+};
 
 void init_ds() {
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++ ) {
 		for (int j = 0; j < M; j++) {
 			visit[i][j] = false;
 		}
 	}
 }
 
-struct info {
-	int i;
-	int j;
-	int cnt;
-};
-
 int bfs(int si, int sj) {
 	init_ds();
+	int ret = 0;
 	queue<info> q;
-	q.push({ si, sj, 0 });
-	visit[si][sj] = true;
-
+	q.push({si, sj, 0});
 	while (!q.empty()) {
 		int hi = q.front().i;
 		int hj = q.front().j;
-		int cur_cnt = q.front().cnt;
+		int cnt = q.front().cnt;
 		q.pop();
 
-		if (map[hi][hj]) return cur_cnt;
-
+		if (map[hi][hj]) {
+			return cnt;
+		}
 		for (int k = 0; k < 8; k++) {
 			int ni = hi + didj[k][0];
 			int nj = hj + didj[k][1];
 			if (ni < 0 || ni >= N || nj < 0 || nj >= M || visit[ni][nj]) continue;
 			visit[ni][nj] = true;
-			q.push({ ni, nj, cur_cnt + 1 });
+			q.push({ ni, nj, cnt + 1 });
 		}
 	}
+	return -1;
 }
 
 int main() {

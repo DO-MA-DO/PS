3c072f7ec444a91027a9381fa6656edbf32f42d2
DOMADO <62499126+DO-MA-DO@users.noreply.github.com>
2023-03-20 01:40:58 +0900

    [Gold V] Title: 소트 게임, Time: 32 ms, Memory: 5012 KB -BaekjoonHub

---
+++ 백준/Gold/1327. 소트 게임/소트 게임.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+int N, K;
+unordered_map<string, bool> um;
+string startstring = "";
+string endstring = "";
+int max_idx;
+
+int bfs() {
+	queue<pair<string, int>> q;
+	q.push({startstring, 0});
+	um.insert({ startstring, true });
+
+	while (!q.empty()) {
+		string cur = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
+
+		if (cur == endstring) {
+			return cnt;
+		}
+
+		for (int i = 0; i <= max_idx; i++) {
+			string newstring(cur);
+			for (int j = 0; j < (K / 2); j++) {
+				swap(newstring[i + j], newstring[i + K - 1 - j]);
+			}
+			if (um.find(newstring) == um.end()) {
+				um.insert({newstring, true});
+				q.push({ newstring, cnt + 1 });
+			}
+		}
+	}
+	return -1;
+}
+
+int main() {
+	scanf("%d%d", &N, &K);
+	max_idx = N - K;
+	
+	char tmp;
+	for (int i = 0; i < N; i++) {
+		scanf(" %c", &tmp);
+		startstring += tmp;
+	}
+	
+	for (int i = 1; i <= N; i++) {
+		endstring += (i + '0');
+	}
+	printf("%d", bfs());
+}

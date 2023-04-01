ba3af5e7c46a66b09a3ef4d384a92edaf2b17bd1
DOMADO <62499126+DO-MA-DO@users.noreply.github.com>
2023-04-01 19:56:30 +0900

    [Silver III] Title: 칸토어 집합, Time: 8 ms, Memory: 3664 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+string str = "";
+
+void solve(int si, int len) {
+	if (len == 1) return;
+
+	int mi = si + len / 3;
+	for (int i = mi; i < mi + len / 3; i++) {
+		str[i] = ' ';
+	}
+	solve(si, len/3);
+	solve(mi + len / 3, len / 3);
+}
+
+int main() {
+	int N;
+	while (scanf("%d", &N) != EOF) {
+		str = "";
+		int cnt = pow(3, N);
+		while (cnt--) {
+			str += '-';
+		}
+		solve(0, pow(3, N));
+		printf("%s\n", str.c_str());
+	}
+}
